834f495a2e11cc661224b3112afa427fd4bd0932
saj528 <ajmarksboy@gmail.com>
2018-04-07 18:56:23 +0000

    destory security

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update. :destroy]
 
 
   def index
@@ -39,6 +39,11 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
