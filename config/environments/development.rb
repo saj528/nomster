57b0f8c8e2901b5fd0ed9d559ee1232f8f84bbe5
saj528 <ajmarksboy@gmail.com>
2018-04-18 17:49:57 +0000

    mailer tester ls43

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,15 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  #config.action_mailer.smtp_settings = {
+    #address: "smtp.gmail.com",
+    #port: 587,
+    #domain: "example.com",
+    #authentication: "plain",
+    #enable_starttls_auto: true,
+    #user_name: ENV['GMAIL_ADDRESS'],
+    #password: ENV['GMAIL_PASSWORD']
+  #}
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # In the development environment your application's code is reloaded on
@@ -56,6 +56,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :test
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
